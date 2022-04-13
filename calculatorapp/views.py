36da114c2bcbda6e96fe1e9c142effcc830aad51
Prabhratirastogi <prabhrati17@gmail.com>
2022-04-13 13:09:14 +0530

    first commit

---
+++ calculatorapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+
+# def submitquery(request):
+#     q = request.GET['query']
+#     Jsondict={
+#         "q":q
+#     }
+#     return JsonResponse(Jsondict)
+
+
+def submitquery(request):
+    q = request.GET['query']
+    try:
+        ans = eval(q)
+        mydictionary = {
+            "q":q,
+            "ans":ans,
+            "error":False
+        }
+        return render(request,'index.html',context = mydictionary)
+    except:
+        mydictionary = {
+            "error":True
+        }
+        return render(request,'index.html',context = mydictionary)
+  
